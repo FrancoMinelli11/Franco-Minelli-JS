c0ee64cceafc3f7ab42639c331c31c60a3c081eb
FrancoMinelli11 <francominelli11@gmail.com>
2024-05-27 20:39:00 -0300

    Añadido de mejoras

--- js/index.js
+++ js/index.js
@@ -18,8 +18,9 @@ function promos(){
     do{
     let oferta = prompt("Escriba cuál oferta es la que busca(Buzos - Pantalones - Nada)").toLowerCase().trim();
     if(oferta === "buzos"){
+        alert("Los buzos están a $10000 en efectivo y $12000 con tarjeta")
         while(BUZ){
-    let tipoBuzo = prompt("¿Cúal color de buzo le gustaría? (Buzo rojo - Buzo azul - Buzo verde)")
+    let tipoBuzo = prompt("¿Cúal color de buzo le gustaría comprar? (Buzo rojo - Buzo azul - Buzo verde - Ninguno)")
     if(tipoBuzo === "Buzo rojo"){
         alert(`El artículo ${tipoBuzo} a sido añadido a su compra`);
         console.log(`Tiene ${cantidadBR++} ${tipoBuzo}`);
@@ -35,15 +36,19 @@ function promos(){
         console.log(`Tiene ${cantidadBV++} ${tipoBuzo}`);
         break
     }
-    else if(tipoBuzo != "Buzo rojo" && tipoBuzo != "Buzo azul" && tipoBuzo != "Buzo verde"){
+    else if(tipoBuzo === "Ninguno"){
+        break
+    }
+    else if(tipoBuzo != "Buzo rojo" && tipoBuzo != "Buzo azul" && tipoBuzo != "Buzo verde" && tipoBuzo != "Ninguno"){
         alert("Ingrese un tipo de buzo válido")
     }
 }
 continuar = prompt("¿Desea seguir comprando?").toLowerCase().trim();
     }
 else if(oferta === "pantalones"){
+    alert("Los pantalones están a $7000 en efectivo y $10000 con tarjeta")
     while (PANT) {
-        let tipoPant = prompt("¿Que tipo de pantalón le gustaría? (Jean - Cargo - Joggin)")
+        let tipoPant = prompt("¿Que tipo de pantalón le gustaría comprar? (Jean - Cargo - Joggin - Ninguno)")
     if(tipoPant === "Jean"){
         alert(`El artículo ${tipoPant} a sido añadido a su compra`);
         console.log(`Tiene ${cantidadJ++} ${tipoPant}`);
@@ -59,7 +64,10 @@ else if(oferta === "pantalones"){
         console.log(`Tiene ${cantidadJo++} ${tipoPant}`);
         break
     }
-    else if(tipoPant != "Jean" && tipoPant != "Joggin" && tipoPant != "Cargo"){
+    else if (tipoPant === "Ninguno"){
+        break
+    }
+    else if(tipoPant != "Jean" && tipoPant != "Joggin" && tipoPant != "Cargo" && tipoBuzo != "Ninguno"){
         alert("Ingrese un tipo de pantalon válido")
     }
 }
